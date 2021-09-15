e8af2c1e8be16aa7fca5f86514c6b0f9cd9bdf55
Said Torres <sesaid17@hotmail.com>
2021-09-15 17:36:53 -0500

    Cleaned code of questions.tsx

--- src/components/questions/questions.tsx
+++ src/components/questions/questions.tsx
@@ -13,61 +13,40 @@ const QuestionsScreen: FC = () => {
   const [questions, setQuestions] = useState<QuestionData[]>()
   const [actualQuestion, setActualQuestion] = useState<ActualQuestion>()
   const [questionIndex, setQuestionIndex] = useState<number>(0)
-  
+
   const [registeredAnswers, setRegisteredAnswers] = useState<any[]>()
   const [finished, setFinished] = useState<boolean>(false);
 
   useEffect(() => {
+    // When the component load, set the data from the json file.
     setQuestions(data.questions)
     setActualQuestionWithQuestionData(data.questions[0])
     // eslint-disable-next-line
   }, [])
 
-  const setActualQuestionWithQuestionData = (questionInQuestionDataForm: QuestionData) => {
-    let answers: ActualQuestion['answers']
-    let question: ActualQuestion['question']
-
-    if (language === Languages.spanish) {
-      question = questionInQuestionDataForm.question.es
-      answers = questionInQuestionDataForm.answers.map((answer) => {
-        let answerLabel = answer.value;
-        if (answer.es) answerLabel = answer.es
-        return { value: answer.value, label: answerLabel }
-      })
-      setActualQuestion({img: questionInQuestionDataForm.img, answers, question})
-    }
-
-    if (language === Languages.english) {
-      question = questionInQuestionDataForm.question.en
-      answers = questionInQuestionDataForm.answers.map((answer) => {
-        let answerLabel = answer.value;
-        if (answer.es) answerLabel = answer.en
-        return { value: answer.value, label: answerLabel }
-      })
-      setActualQuestion({img: questionInQuestionDataForm.img, answers, question})
-    }
-  }
-
   useEffect(() => {
+    // On change question index, the actual question changes to it's correspondiente index
     if (!questions?.length) return
     setActualQuestionWithQuestionData(questions[questionIndex])
     // eslint-disable-next-line
   }, [questionIndex, questions, language])
 
-  useEffect(() => {
-    console.log(registeredAnswers)
-  }, [registeredAnswers])
+  const setActualQuestionWithQuestionData = (questionInQuestionDataForm: QuestionData) => {
+    if (language === Languages.spanish) {
+      setActualQuestionInSpanish(questionInQuestionDataForm)
+    }
+    if (language === Languages.english) {
+      setActualQuestionInEnglish(questionInQuestionDataForm)
+    }
+  }
 
   const storeThisAnswer = (answer: any) => {
     if (registeredAnswers?.length) {
-      if (typeof registeredAnswers[questionIndex] === 'undefined') {
-        const newArray = [...registeredAnswers]
-        newArray.push(answer)
-        setRegisteredAnswers(newArray)
+      const existARegisteredAnswerInThisQuestion = typeof registeredAnswers[questionIndex] !== 'undefined'
+      if (!existARegisteredAnswerInThisQuestion) {
+        pushAnswerToRegisteredAnswers(answer)
       } else {
-        const newArray = [...registeredAnswers]
-        newArray[questionIndex] = answer
-        setRegisteredAnswers(newArray)
+        replaceAnswerOfRegisteredAnswers(answer)
       }
     } else {
       setRegisteredAnswers([answer])
@@ -75,6 +54,40 @@ const QuestionsScreen: FC = () => {
   }
 
   const handleNext = (answer: any) => {
+    storeAnswerAndSetNextQuestion(answer)
+  }
+
+  const handlePrev = (answer: any) => {
+    storeAnswerAndSetPrevQuestion(answer)
+  }
+
+  // UTILITIES *******************************************************************************************************
+
+  const setActualQuestionInSpanish = (questionInQuestionDataForm: QuestionData) => {
+    let answers: ActualQuestion['answers']
+    let question: ActualQuestion['question']
+    question = questionInQuestionDataForm.question.es
+    answers = questionInQuestionDataForm.answers.map((answer) => {
+      let answerLabel = answer.value;
+      if (answer.es) answerLabel = answer.es
+      return { value: answer.value, label: answerLabel }
+    })
+    setActualQuestion({ img: questionInQuestionDataForm.img, answers, question })
+  }
+
+  const setActualQuestionInEnglish = (questionInQuestionDataForm: QuestionData) => {
+    let answers: ActualQuestion['answers']
+    let question: ActualQuestion['question']
+    question = questionInQuestionDataForm.question.en
+    answers = questionInQuestionDataForm.answers.map((answer) => {
+      let answerLabel = answer.value;
+      if (answer.es) answerLabel = answer.en
+      return { value: answer.value, label: answerLabel }
+    })
+    setActualQuestion({ img: questionInQuestionDataForm.img, answers, question })
+  }
+
+  const storeAnswerAndSetNextQuestion = (answer: any) => {
     const nextIndex = questionIndex + 1
     if (!questions?.length) return
 
@@ -83,13 +96,81 @@ const QuestionsScreen: FC = () => {
     setQuestionIndex(nextIndex)
   }
 
-  const handlePrev = (answer: any) => {
+  const storeAnswerAndSetPrevQuestion = (answer: any) => {
     const prevIndex = questionIndex - 1
     if (!questions?.length) return
 
     if (typeof answer !== 'undefined') storeThisAnswer(answer)
     if (prevIndex < 0) return
     setQuestionIndex(prevIndex)
+  }
+
+  const pushAnswerToRegisteredAnswers = (answer: any) => {
+    if (!registeredAnswers?.length) return
+    const existARegisteredAnswerInThisQuestion = typeof registeredAnswers[questionIndex] !== 'undefined'
+    if (!existARegisteredAnswerInThisQuestion) {
+      const newArray = [...registeredAnswers]
+      newArray.push(answer)
+      setRegisteredAnswers(newArray)
+    }
+  }
+
+  const replaceAnswerOfRegisteredAnswers = (answer: any) => {
+    if (!registeredAnswers?.length) return
+    const newArray = [...registeredAnswers]
+    newArray[questionIndex] = answer
+    setRegisteredAnswers(newArray)
+  }
+
+  // END OF UTILITIES *******************************************************************************************************
+
+  const QuestionScreenQuestion: FC = () => {
+    return (
+      <div className="questions-screen__question">
+        <header className="questions-screen__question__question-counter">
+          <div className="questions-screen__question__question-counter__txt">
+            {questionIndex + 1}/{data.questions.length || 0}
+          </div>
+          <div className="questions-screen__question__question-counter__progress-bar">
+            <div
+              className="questions-screen__question__question-counter__progress-bar__progress"
+              style={{ width: questions?.length ? ((((questionIndex + 1) / questions.length) * 100) + '%') : undefined }}
+            />
+          </div>
+          <button
+            onClick={() => { language === Languages.spanish ? setLanguage(Languages.english) : setLanguage(Languages.spanish) }}
+            className="questions-screen__question__question-counter__lang-button"
+          >
+            {language === Languages.spanish ? "Change language" : "Cambiar idioma"}
+          </button>
+        </header>
+        <Question
+          question={actualQuestion?.question || ""}
+          answers={actualQuestion?.answers || [{ value: '' }]}
+          onNext={answer => handleNext(answer)}
+          prevAnswer={registeredAnswers?.length ? registeredAnswers[questionIndex] : undefined}
+          onPrev={answer => handlePrev(answer)}
+          language={language}
+        />
+      </div>
+    )
+  }
+
+  const QuestionScreenFinished: FC = () => {
+    return (
+      <div className="questions-screen__finished">
+        <h1 className="questions-screen__finished__thxTxt">{language === Languages.spanish ? '¡Gracias por contestar!' : 'Thanks for answering!'}</h1>
+        {/* <img className="questions-screen__finished__gif" src="https://i.giphy.com/media/l1INk1qF0fw73snz2p/giphy.webp" alt="" /> */}
+        {
+          questions && registeredAnswers ?
+            <QuestionsWithAnswersInTable
+              questions={questions}
+              answeredQuestions={registeredAnswers}
+              language={language}
+            /> : undefined
+        }
+      </div>
+    )
   }
 
   return (
@@ -103,47 +184,10 @@ const QuestionsScreen: FC = () => {
       } : undefined}
     >
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <button
-        onClick={()=>{language===Languages.spanish ? setLanguage(Languages.english) : setLanguage(Languages.spanish)}}
-        style={{height: '3vh', width: '18vh', fontSize: '1.60vh', padding: '0.1vh', borderWidth: '0.1vh'}}
-      >
-        {language === Languages.spanish ? "Change language" : "Cambiar idioma"}
-      </button>
       {!finished ?
-        <div className="questions-screen__question">
-          <header className="questions-screen__question__question-counter">
-            <div className="questions-screen__question__question-counter__txt">
-              {questionIndex + 1}/{data.questions.length || 0}
-            </div>
-            <div className="questions-screen__question__question-counter__progress-bar">
-              <div
-                className="questions-screen__question__question-counter__progress-bar__progress"
-                style={{ width: questions?.length ? ((((questionIndex + 1) / questions.length) * 100) + '%') : undefined }}
-              />
-            </div>
-          </header>
-          <Question
-            question={actualQuestion?.question || ""}
-            answers={actualQuestion?.answers || [{ value: '' }]}
-            onNext={answer => handleNext(answer)}
-            prevAnswer={registeredAnswers?.length ? registeredAnswers[questionIndex] : undefined}
-            onPrev={answer => handlePrev(answer)}
-            language={language}
-          />
-        </div>
+        <QuestionScreenQuestion />
         :
-        <div className="questions-screen__finished">
-          <h1 className="questions-screen__finished__thxTxt">{language === Languages.spanish ? '¡Gracias por contestar!' : 'Thanks for answering!'}</h1>
-          {/* <img className="questions-screen__finished__gif" src="https://i.giphy.com/media/l1INk1qF0fw73snz2p/giphy.webp" alt="" /> */}
-          {
-            questions && registeredAnswers ?
-              <QuestionsWithAnswersInTable
-                questions={questions}
-                answeredQuestions={registeredAnswers}
-                language={language}
-              /> : undefined
-          }
-        </div>
+        <QuestionScreenFinished />
       }
     </body>
   )
