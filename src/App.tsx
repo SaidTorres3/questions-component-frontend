5e8080a5cf370798a54c2c31bad6e525705c96ea
Said Torres <sesaid17@hotmail.com>
2021-10-05 18:03:00 -0500

    Added admin dashboard frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Admin from './components/admin/layouts/Admin';
 import QuestionsScreen from 'src/components/client/questions/questions';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={QuestionsScreen}></Route>
+          <Route exact path="/" component={QuestionsScreen} />
+          <Route path="/admin/dashboard" component={Admin} />
+          <Redirect exact from="/admin" to="/admin/dashboard" />
+          <Route path="/admin/" component={Admin} />
           <Route path="*" component={QuestionsScreen}></Route>
         </Switch>
       </BrowserRouter>
---
+++ src/routes.ts
@@ -0,0 +1,97 @@
+// @material-ui/icons
+import Dashboard from '@material-ui/icons/Dashboard';
+import Person from '@material-ui/icons/Person';
+import LibraryBooks from '@material-ui/icons/LibraryBooks';
+import BubbleChart from '@material-ui/icons/BubbleChart';
+import LocationOn from '@material-ui/icons/LocationOn';
+import Notifications from '@material-ui/icons/Notifications';
+import Unarchive from '@material-ui/icons/Unarchive';
+import Language from '@material-ui/icons/Language';
+// core components/views for Admin layout
+import DashboardPage from './components/admin/views/Dashboard/Dashboard';
+import UserProfile from './components/admin/views/UserProfile/UserProfile';
+import TableList from './components/admin/views/TableList/TableList';
+import Typography from './components/admin/views/Typography/Typography';
+import Icons from './components/admin/views/Icons/Icons';
+import Maps from './components/admin/views/Maps/Maps';
+import NotificationsPage from './components/admin/views/Notifications/Notifications';
+import UpgradeToPro from './components/admin/views/UpgradeToPro/UpgradeToPro';
+// core components/views for RTL layout
+import RTLPage from './components/admin/views/RTLPage/RTLPage';
+
+const dashboardRoutes = [
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    rtlName: 'لوحة القيادة',
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: '/admin'
+  },
+  {
+    path: '/user',
+    name: 'User Profile',
+    rtlName: 'ملف تعريفي للمستخدم',
+    icon: Person,
+    component: UserProfile,
+    layout: '/admin'
+  },
+  {
+    path: '/table',
+    name: 'Table List',
+    rtlName: 'قائمة الجدول',
+    icon: 'content_paste',
+    component: TableList,
+    layout: '/admin'
+  },
+  {
+    path: '/typography',
+    name: 'Typography',
+    rtlName: 'طباعة',
+    icon: LibraryBooks,
+    component: Typography,
+    layout: '/admin'
+  },
+  {
+    path: '/icons',
+    name: 'Icons',
+    rtlName: 'الرموز',
+    icon: BubbleChart,
+    component: Icons,
+    layout: '/admin'
+  },
+  {
+    path: '/maps',
+    name: 'Maps',
+    rtlName: 'خرائط',
+    icon: LocationOn,
+    component: Maps,
+    layout: '/admin'
+  },
+  {
+    path: '/notifications',
+    name: 'Notifications',
+    rtlName: 'إخطارات',
+    icon: Notifications,
+    component: NotificationsPage,
+    layout: '/admin'
+  },
+  {
+    path: '/upgrade-to-pro',
+    name: 'Upgrade To PRO',
+    rtlName: 'التطور للاحترافية',
+    icon: Unarchive,
+    component: UpgradeToPro,
+    layout: '/admin'
+  },
+  {
+    path: '/rtl-page',
+    name: 'RTL Support',
+    rtlName: 'پشتیبانی از راست به چپ',
+    icon: Language,
+    component: RTLPage,
+    layout: '/rtl'
+  }
+];
+
+export default dashboardRoutes;
